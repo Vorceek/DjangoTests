8a4d5f205ba30e4c1efcd45d60411660f9aef01e
Vinicios <vorceek@gmail.com>
2025-02-02 13:54:18 -0300

    Banco de dados e primeiro migrate

--- timesheet/core/settings.py
+++ timesheet/core/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DjangoTests',
+        'USER': 'postgres',
+        'PASSWORD': '4849',
+        'HOST': 'localhost',
+        'PORT': '5433', 
     }
 }
 
