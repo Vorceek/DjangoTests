1eb50c8603db7b0eb43050d8aa6bdaeb34e07576
Vinicios <vorceek@gmail.com>
2025-02-14 18:00:54 -0300

    Novo Estilo

--- timesheet/apps/base_app/permissions.py
+++ timesheet/apps/base_app/permissions.py
@@ -27,7 +27,7 @@ def aside_icons(request):
     icons = [
         {'link': '/admin-base/', 'icon_class': 'fa-solid fa-house', 'is_admin': True},
         {'link': '/admin-base/relatorio/', 'icon_class': 'fa-solid fa-file-lines', 'is_admin': True},
-        {'link': '/upload/', 'icon_class': 'fa-solid fa-magnifying-glass', 'is_admin': True},
+        #{'link': '/upload/', 'icon_class': 'fa-solid fa-magnifying-glass', 'is_admin': True},
 
     ]
     
