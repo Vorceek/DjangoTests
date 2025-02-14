9677a4a74df3fe22555c600c7f7413add801051c
Vinicios <vorceek@gmail.com>
2025-02-14 16:12:51 -0300

    Campos Relacionados no Filtro

--- timesheet/apps/atividade_app/admin.py
+++ timesheet/apps/atividade_app/admin.py
@@ -4,10 +4,10 @@ from django.utils.timezone import localtime
 
 # REGISTRO ATIVIDADE
 class RegistroAtividadeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'RAM_cliente', 'RAM_servico', 'RAM_atividade', 'hora_formatada', 'RAM_dataFinal', 'RAM_duracao')
-    list_display_links = ('id', 'RAM_cliente', 'RAM_servico')
+    list_display = ('id', 'RAM_colaborador', 'RAM_cliente', 'RAM_servico', 'RAM_atividade', 'hora_formatada', 'RAM_dataFinal', 'RAM_duracao')
+    list_display_links = ('id', 'RAM_colaborador', 'RAM_cliente', 'RAM_servico')
     fields = ['RAM_cliente', 'RAM_servico', 'RAM_atividade']
-    list_filter = ('RAM_cliente', 'RAM_servico', 'RAM_atividade', 'RAM_dataInicial')
+    list_filter = ('RAM_colaborador', 'RAM_cliente', 'RAM_servico', 'RAM_atividade', 'RAM_dataInicial')
 
     def hora_formatada(self, obj):
         return localtime(obj.RAM_dataInicial).strftime('%d/%m/%Y %H:%M')
--- timesheet/apps/atividade_app/endpoints.py
+++ timesheet/apps/atividade_app/endpoints.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from .models import Cliente, Servico, Atividade
 
 def get_servicos_por_cliente(request):
-    cliente_ids = request.GET.getlist('cliente_ids[]')  # IDs dos clientes selecionados
+    cliente_ids = request.GET.getlist('cliente_ids')  # IDs dos clientes selecionados
     servicos = Servico.objects.filter(clientes__id__in=cliente_ids).distinct()
     
     servicos_data = [{"id": servico.id, "nome": servico.nome} for servico in servicos]
@@ -10,7 +10,7 @@ def get_servicos_por_cliente(request):
     return JsonResponse({"servicos": servicos_data})
 
 def get_atividades_por_servico(request):
-    servico_ids = request.GET.getlist('servico_ids[]')  # IDs dos serviços selecionados
+    servico_ids = request.GET.getlist('servico_ids')  # IDs dos serviços selecionados
     atividades = Atividade.objects.filter(servicos__id__in=servico_ids).distinct()
     
     atividades_data = [{"id": atividade.id, "nome": atividade.nome} for atividade in atividades]
@@ -18,7 +18,7 @@ def get_atividades_por_servico(request):
     return JsonResponse({"atividades": atividades_data})
 
 def get_servicos_por_atividade(request):
-    atividade_ids = request.GET.getlist('atividade_ids[]')  # IDs das atividades selecionadas
+    atividade_ids = request.GET.getlist('atividade_ids')  # IDs das atividades selecionadas
     servicos = Servico.objects.filter(atividades__id__in=atividade_ids).distinct()
     
     servicos_data = [{"id": servico.id, "nome": servico.nome} for servico in servicos]
