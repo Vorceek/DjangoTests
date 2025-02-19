5f94eb0fd9f9333534619cd460d0994f5959bc0d
Vinicios <vorceek@gmail.com>
2025-02-19 14:33:31 -0300

    New Icons

--- timesheet/apps/base_app/permissions.py
+++ timesheet/apps/base_app/permissions.py
@@ -25,9 +25,9 @@ def redirect_based_on_group(user):
 def aside_icons(request):
     # Defina os ícones desejados:
     icons = [
-        #{'link': '/admin-base/', 'icon_class': 'fa-solid fa-house', 'is_admin': True},
-        {'link': '/admin-base/relatorio/', 'icon_class': 'fa-solid fa-file-lines', 'is_admin': True},
-        #{'link': '/upload/', 'icon_class': 'fa-solid fa-magnifying-glass', 'is_admin': True},
+        {'link': '/user-base/', 'icon_name': 'plus-circle', 'is_admin': True},
+        {'link': '/admin-base/relatorio/', 'icon_name': 'file-text', 'is_admin': True},
+        #{'link': '/admin-base/relatorio/', 'icon_name': 'file-text', 'is_admin': True},
 
     ]
     
